2cafc766f5036c56f1838d209c4c73afe8757931
janerli <dolgoruk.alina@gmail.com>
2024-12-23 01:56:07 +0300

    c

--- 6.py
+++ 6.py
@@ -3,9 +3,6 @@
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 
-import tkinter as tk
-from tkinter import ttk, filedialog, messagebox
-
 
 class NameApp:
     def __init__(self, window):
